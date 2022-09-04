e314c8e3b2881ea308d433f1ba5e02dd8cad3e10
Nguyễn Văn Phát <nguyenvanphat@MacBook-Pro-cua-Nguyen.local>
2022-09-04 21:16:10 +0700

    hehe

--- detection_helpers.py
+++ detection_helpers.py
@@ -114,6 +114,10 @@ class Detector:
         # "img" có type = <class 'numpy.ndarray'>; shape = (3, 384, 640);
         # "im0" có type = <class 'numpy.ndarray'>; shape = (1080, 1920, 3);
         img, im0 = self.load_image(source)
+        pprint("img", img)
+        pprint("img", self.img)
+        self.pprint("img", img)
+        self.pprint("img", self.img)
         img = torch.from_numpy(img).to(self.device)
         img = img.half() if self.half else img.float()  # uint8 to fp16/32
         img /= 255.0  # 0 - 255 to 0.0 - 1.0
@@ -154,9 +158,7 @@ class Detector:
         pred = non_max_suppression(pred, self.conf_thres, self.iou_thres, classes=self.classes, agnostic=self.agnostic_nms)
 
         # Apply Classifier
-        print("\nhaha_first")
-        print(self.classify)
-        self.pprint("self.classify", self.classify)
+        # "classify" mặc định là False
         if self.classify:
             pred = apply_classifier(pred, self.modelc, img, im0) # I thnk we need to add a new axis to im0
 
