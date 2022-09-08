13a638e038a296a6a637b4dabaf199d8ea30dd58
Nguyễn Văn Phát <nguyenvanphat@192.168.2.2>
2022-09-08 15:30:36 +0700

    hehe

--- test_Phat.py
+++ test_Phat.py
@@ -1,1 +1,8 @@
-print(round(3.1))
+origin_fps = 20
+stride_fps = 5
+fps_video_src = 30.0
+
+origin_frame_throughout = 2
+stride_frame_throughout = 3
+
+print(int(origin_frame_throughout + (stride_frame_throughout*((int(fps_video_src)-origin_fps)/stride_fps))))
